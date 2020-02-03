f9be96fbab59914600f043ef0bff8439c8656c5d
Mark Verrips <2083591@talnet.nl>
2020-02-03 10:15:51 +0100

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,11 @@
+<?php
+echo("vul een getal in: ");
+$nummer = readline();
+if (($nummer % 2) == 0){
+        echo("het getal is even");
+} if(($nummer & 1) == 1)  {
+        echo("het getal is oneven");
+
+}
+?>
+git add -A git commit -am "Uitwerking van de exercise" git push
